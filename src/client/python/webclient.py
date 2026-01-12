1eb70529908aa30c032e4c0421e84df026ce3c1d
gerald-scharitzer <gerald.scharitzer@gmail.com>
2026-01-13 00:04:05 +0100

    get_ciphers

--- src/client/python/webclient.py
+++ src/client/python/webclient.py
@@ -1,29 +1,41 @@
 import argparse
+import ssl
 import requests
 
 USAGE = "webclient.py"
 
 def main():
-    url = "https://localhost:8443"
-    DEFAULT_TRUST_PATH = "src/front/conf/cert.pem"
+	url = "https://localhost:8443"
+	DEFAULT_TRUST_PATH = "src/front/conf/cert.pem"
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--trust",
-        type=str,
-        default=DEFAULT_TRUST_PATH,
-        help=f"Path to the trusted TLS certificates (default: {DEFAULT_TRUST_PATH})",
-    )
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		"--trust",
+		type=str,
+		default=DEFAULT_TRUST_PATH,
+		help=f"Path to the trusted TLS certificates (default: {DEFAULT_TRUST_PATH})",
+	)
+	args = parser.parse_args()
 
-    try:
-        r = requests.get(url, verify=args.trust) # FIXME verify
-    except requests.exceptions.SSLError as e:
-        print(f"TLS Error: {e}")
-        return
-    
-    print(r.status_code)
-    print(f"GET {url}")
+	print(f"TLS version: {ssl.OPENSSL_VERSION}")
+	ssl_context = get_ssl_context()
+	ciphers = ssl_context.get_ciphers()
+	print("Client ciphers:")
+	for cipher in ciphers:
+		print(cipher['description'])
+
+	try:
+		r = requests.get(url, verify=args.trust) # FIXME verify
+	except requests.exceptions.SSLError as e:
+		print(f"TLS Error: {e}")
+		return
+	
+	print(r.status_code)
+	print(f"GET {url}")
+
+def get_ssl_context() -> ssl.SSLContext:
+	context = ssl.create_default_context()
+	return context
 
 if __name__ == "__main__":
-    main()
+	main()
--- src/jekyll/python.md
+++ src/jekyll/python.md
@@ -6,7 +6,7 @@ lastUpdate: 2026-01-07
 # Python
 
 web clients based on the standard library can use the module [`http.client`](https://docs.python.org/3/library/http.client.html) for HTTPS requests, where HTTPS depends on the module [`ssl`](https://docs.python.org/3/library/ssl.html).
-This in turn depends on OpenSSL and its resolution at runtime can be read from [`ssl.OPENSSL_VERSION`](https://docs.python.org/3/library/ssl.html#ssl.OPENSSL_VERSION).
+This in turn depends on OpenSSL and its resolution at runtime can be read from [`ssl.OPENSSL_VERSION`](https://docs.python.org/3/library/ssl.html#ssl.OPENSSL_VERSION) or its variants.
 
 ## Requests module
 
