6ec862ba3aa0eaa91653f57399c553a41d00e1c3
gerald-scharitzer <gerald.scharitzer@gmail.com>
2026-01-13 23:49:16 +0100

    rebuild

--- sh/build-python-webclient.sh
+++ sh/build-python-webclient.sh
@@ -1,3 +1,4 @@
 # Build the Python web client image
 . sh/conf.sh &&
+. sh/remove-python-web-client-image.sh &&
 podman build -t python-web-client:$VERSION src/client/python
--- sh/remove-python-web-client-image.sh
+++ sh/remove-python-web-client-image.sh
@@ -1,3 +1,3 @@
 # Remove the Python web client image and its containers
 . sh/conf.sh &&
-podman rmi --force python-web-client:$VERSION
+podman rmi --force python-web-client:$VERSION # TODO upgrade podman to use --ignore
