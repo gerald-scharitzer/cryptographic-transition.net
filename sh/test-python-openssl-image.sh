9e92d9064eab5b1e3f18347d126396aeab61db89
gerald-scharitzer <gerald.scharitzer@gmail.com>
2026-01-17 23:41:40 +0100

    openssl list

--- sh/test-python-openssl-image.sh
+++ sh/test-python-openssl-image.sh
@@ -1,4 +1,7 @@
 # Test the Python with OpenSSL image
 . sh/conf.sh &&
 podman run --replace -a stdout --name python-openssl python-openssl:$VERSION python -V &&
-podman run --replace -a stdout --name python-openssl python-openssl:$VERSION openssl version
+podman run --replace -a stdout --name python-openssl python-openssl:$VERSION openssl version &&
+podman run --replace -a stdout --name python-openssl python-openssl:$VERSION openssl list -kem-algorithms &&
+podman run --replace -a stdout --name python-openssl python-openssl:$VERSION openssl list -signature-algorithms &&
+podman run --replace -a stdout --name python-openssl python-openssl:$VERSION openssl list -public-key-algorithms
