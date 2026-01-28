cc234aa9f4b62c3911d5025667e4b91650dba449
gerald-scharitzer <gerald.scharitzer@gmail.com>
2026-01-28 23:36:07 +0100

    nginx vs s_server

--- sh/test-openssl-image.sh
+++ sh/test-openssl-image.sh
@@ -1,10 +1,11 @@
 # Test the OpenSSL image
-. sh/conf.sh &&
-podman run --replace -a stdout --name openssl openssl:$VERSION openssl version &&
-podman run --replace -a stdout --name openssl openssl:$VERSION openssl list -kem-algorithms &&
-podman run --replace -a stdout --name openssl openssl:$VERSION openssl list -signature-algorithms &&
-podman run --replace -a stdout --name openssl openssl:$VERSION openssl list -public-key-algorithms &&
-podman run --replace -a stdout --name openssl openssl:$VERSION openssl ciphers &&
-podman run --replace -a stdout --name openssl openssl:$VERSION openssl info -configdir &&
+set -e
+. sh/conf.sh
+podman run --replace -a stdout --name openssl openssl:$VERSION openssl version
+podman run --replace -a stdout --name openssl openssl:$VERSION openssl list -kem-algorithms
+podman run --replace -a stdout --name openssl openssl:$VERSION openssl list -signature-algorithms
+podman run --replace -a stdout --name openssl openssl:$VERSION openssl list -public-key-algorithms
+podman run --replace -a stdout --name openssl openssl:$VERSION openssl ciphers
+podman run --replace -a stdout --name openssl openssl:$VERSION openssl info -configdir
 server_id=$(podman run --replace --detach --name openssl-s_server openssl:$VERSION openssl s_server -4 -accept '127.0.0.1:8443' -cert /etc/ssl/server.pem -key /etc/ssl/key.pem)
 echo $server_id
--- src/jekyll/openssl.md
+++ src/jekyll/openssl.md
@@ -1,7 +1,7 @@
 ---
 layout: default
 title: OpenSSL @ cryptographic-transition.net
-lastUpdate: 2025-12-08
+lastUpdate: 2026-01-28
 ---
 # OpenSSL
 
@@ -15,4 +15,7 @@ OpenSSL provides TLS for several major software projects.
 
 [Python](python.html) clients use the installed OpenSSL.
 
-Nginx [web servers](web-server.html) uses the installed OpenSSL.
+Nginx [web servers](web-server.html) use the installed OpenSSL.
+
+The OpenSSL `s_server` command runs a TLS server, but the [documentation of `s_server`](https://docs.openssl.org/3.6/man1/openssl-s_server/#bugs) itself reads:
+"Because this program has a lot of options and also because some of the techniques used are rather old, the C source for this command is rather hard to read and not a model of how things should be done. A typical SSL server program would be much simpler."
