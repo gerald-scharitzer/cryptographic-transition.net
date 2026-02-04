13e3c10169ee8e1e6e4d98733f180c9cf60b7955
gerald-scharitzer <gerald.scharitzer@gmail.com>
2026-02-04 22:31:14 +0100

    net-container

--- sh/test-openssl-image.sh
+++ sh/test-openssl-image.sh
@@ -1,5 +1,4 @@
 # Test the OpenSSL image
-set -e
 . sh/conf.sh
 podman run --replace -a stdout --name openssl openssl:$VERSION openssl version
 podman run --replace -a stdout --name openssl openssl:$VERSION openssl list -kem-algorithms
@@ -7,5 +6,7 @@ podman run --replace -a stdout --name openssl openssl:$VERSION openssl list -sig
 podman run --replace -a stdout --name openssl openssl:$VERSION openssl list -public-key-algorithms
 podman run --replace -a stdout --name openssl openssl:$VERSION openssl ciphers
 podman run --replace -a stdout --name openssl openssl:$VERSION openssl info -configdir
-server_id=$(podman run --replace --detach --name openssl-s_server openssl:$VERSION openssl s_server -4 -accept '127.0.0.1:8443' -cert /etc/ssl/server.pem -key /etc/ssl/key.pem)
-echo $server_id
+server_id=$(podman run --replace --detach --name openssl-s_server --publish '127.0.0.1:8443:8443' openssl:$VERSION openssl s_server -4 -state -accept '127.0.0.1:8443' -cert /etc/ssl/server.pem -key /etc/ssl/key.pem)
+echo server_id:$server_id
+sleep 1 # FIXME use health or readiness probe instead
+podman run --replace -a stdout,stderr --name openssl --net "container:$server_id" openssl:$VERSION openssl s_client -4 '127.0.0.1:8443'
