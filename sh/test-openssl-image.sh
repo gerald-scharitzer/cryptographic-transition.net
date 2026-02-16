ffd1694dba5e0862f1ed07bc41c1d3d089c319d4
gerald-scharitzer <gerald.scharitzer@gmail.com>
2026-02-16 23:59:31 +0100

    cid

--- sh/test-openssl-image.sh
+++ sh/test-openssl-image.sh
@@ -6,7 +6,12 @@ podman run --replace -a stdout --name openssl openssl:$VERSION openssl list -sig
 podman run --replace -a stdout --name openssl openssl:$VERSION openssl list -public-key-algorithms
 podman run --replace -a stdout --name openssl openssl:$VERSION openssl ciphers
 podman run --replace -a stdout --name openssl openssl:$VERSION openssl info -configdir
-server_id=$(podman run --replace --detach --name openssl-s_server --publish '127.0.0.1:8443:8443' openssl:$VERSION openssl s_server -4 -state -accept '127.0.0.1:8443' -cert /etc/ssl/server.pem -key /etc/ssl/key.pem)
-echo server_id:$server_id
+podman rm --force --ignore openssl
+podman rm --force --ignore openssl-s_server
+rm temp/openssl-s_server.cid
+podman run --replace --detach --name openssl-s_server --cidfile temp/openssl-s_server.cid --publish '127.0.0.1:8443:8443' openssl:$VERSION openssl s_server -4 -state -accept '127.0.0.1:8443' -cert /etc/ssl/server.pem -key /etc/ssl/key.pem
 sleep 1 # FIXME use health or readiness probe instead
+server_id=$(cat temp/openssl-s_server.cid) # FIXME verify variable content
+echo server_id: $server_id
+# FIXME unexpected eof
 podman run --replace -a stdout,stderr --name openssl --net "container:$server_id" openssl:$VERSION openssl s_client -4 '127.0.0.1:8443'
