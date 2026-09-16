9a84777462d7837bd98983bdb703b0e12d2239e6
gerald-scharitzer <gerald.scharitzer@gmail.com>
2026-09-16 22:42:56 +0200

    reflex run

--- src/server/reflex/front/front/front.py
+++ src/server/reflex/front/front/front.py
@@ -12,12 +12,8 @@ def index() -> rx.Component:
     return rx.container(
         rx.color_mode.button(position="top-right"),
         rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
-            ),
+            rx.heading(f"{config.app_name}"),
+            rx.text("Reflex"),
             rx.link(
                 rx.button("Check out our docs!"),
                 href="https://reflex.dev/docs/getting-started/introduction/",
