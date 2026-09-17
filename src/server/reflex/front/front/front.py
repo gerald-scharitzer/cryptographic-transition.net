938a0ac28702f0eb53336650a85baff5275b8d2c
gerald-scharitzer <gerald.scharitzer@gmail.com>
2026-09-17 23:18:59 +0200

    state

--- src/server/reflex/front/front/front.py
+++ src/server/reflex/front/front/front.py
@@ -1,29 +1,31 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
+"""reflex app"""
 
 import reflex as rx
 
 from rxconfig import config
 
 class State(rx.State):
-    """The app state."""
+	"""app state"""
+	name: str = "Reflex"
+
+	@rx.event
+	def set_name(self, value: str):
+		self.name = value
 
 def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading(f"{config.app_name}"),
-            rx.text("Reflex"),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
-            ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-    )
+	# Welcome Page (Index)
+	return rx.container(
+		rx.color_mode.button(position="top-right"),
+		rx.vstack(
+			rx.heading(f"{config.app_name}"),
+			rx.text(State.name),
+			rx.input(placeholder="The new value", on_blur=State.set_name),
+			rx.button("Set", on_click=lambda: State.set_name("true")),
+			spacing="5",
+			justify="center",
+			min_height="85vh",
+		),
+	)
 
 app = rx.App()
 app.add_page(index)
